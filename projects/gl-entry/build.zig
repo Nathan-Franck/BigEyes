fd698d7882324f5b7fab8a544f2d2f87fae92454
Nathan Franck <nathanfranck1@gmail.com>
2023-11-30 14:02:43 -0700

    Cat content

--- projects/gl-entry/build.zig
+++ projects/gl-entry/build.zig
@@ -2,6 +2,63 @@ const std = @import("std");
 const Builder = std.build.Builder;
 const fs = std.fs;
 
+const ExportMeshes = struct {
+    allocator: std.mem.Allocator,
+    files: []const []const u8,
+    step: std.build.Step,
+    pub fn create(b: *std.Build, files: []const []const u8) *ExportMeshes {
+        const self = b.allocator.create(ExportMeshes) catch @panic("OOM");
+        self.* = .{
+            .allocator = b.allocator,
+            .files = files,
+            .step = std.build.Step.init(.{
+                .id = .custom,
+                .name = "export_meshes",
+                .owner = b,
+                .makeFn = exportMeshes,
+            }),
+        };
+        return self;
+    }
+    fn exportMeshes(step: *std.build.Step, prog_node: *std.Progress.Node) !void {
+        _ = prog_node;
+        const self = @fieldParentPtr(ExportMeshes, "step", step);
+
+        var b = step.owner;
+
+        for (self.files) |file| {
+            const full_path = try std.fmt.allocPrint(self.allocator, "content/{s}.blend", .{file});
+
+            var man = b.cache.obtain();
+            defer man.deinit();
+            _ = try man.addFile(full_path, null);
+            if (try step.cacheHit(&man)) {
+                _ = man.final();
+                continue;
+            }
+            _ = man.final();
+            try man.writeManifest();
+
+            std.debug.print("Working on {s}", .{file});
+            var timer = try std.time.Timer.start();
+            const res = try std.ChildProcess.run(.{
+                .allocator = self.allocator,
+                .argv = &[_][]const u8{
+                    "blender",
+                    full_path,
+                    "--background",
+                    "--python",
+                    "content/custom-gltf.py",
+                },
+                .cwd = try std.process.getCwdAlloc(self.allocator),
+            });
+            std.debug.print("stdout: {s}\n", .{res.stdout});
+            const ns = timer.read();
+            std.debug.print("Process took {d} ms\n", .{@as(f64, @floatFromInt(ns)) / 1_000_000});
+        }
+    }
+};
+
 inline fn thisDir() []const u8 {
     return comptime std.fs.path.dirname(@src().file) orelse ".";
 }
@@ -45,6 +102,9 @@ pub fn build(
     exe.linkSystemLibrary("SDL2");
     exe.linkLibC();
     const install_artifact = b.addInstallArtifact(exe, .{});
+
+    const export_meshes = ExportMeshes.create(b, &.{"cat"});
+    exe.step.dependOn(&export_meshes.step);
 
     const zmath_pkg = @import("../../libs/zig-gamedev/libs/zmath/build.zig").package(b, exe.target, exe.optimize, .{});
     zmath_pkg.link(exe);
