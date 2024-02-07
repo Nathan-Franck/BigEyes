417230105c204cacd6b669b070ba0db65f0a0b17
Nathan Franck <nathanfranck1@gmail.com>
2024-02-07 01:08:41 -0700

    Somewhat unmangled, I got my tooling for css sorted

--- web.solid/src/App.tsx
+++ web.solid/src/App.tsx
@@ -3,12 +3,20 @@ import solidLogo from './assets/solid.svg'
 import viteLogo from '/vite.svg'
 
 function declareStyle<const T extends Record<string, Partial<CSSStyleDeclaration>>>(style: T) {
-  const classes = Object.keys(style) as (keyof T)[];
-  const encodedStyle = classes.map((key) => {
+  // Build lookup of class names to class name as a string.
+const classList: (keyof T)[] = Object.keys(style).reduce((acc, key) => {
+  const keys = key.split('.');
+  return [...acc, ...keys];
+}, [] as any);
+  const classes: { [key in keyof T]: key } = classList.reduce((acc, key) => {
+    acc[key as string] = key;
+    return acc;
+  }, {} as any);
+  const encodedStyle = classList.map((key) => {
     const value = style[key];
-    return `.${key} {${Object.keys(value).map((key) => {
+    return `.${key as string} {${Object.keys(value).map((key) => {
       const dashedKey = key.replace(/[A-Z]/g, (match) => `-${match.toLowerCase()}`);
-      return `${dashedKey}: ${value[key]};`
+      return `${dashedKey}: ${value[key as any]};`
     }).join('')}}`
   }).join('');
   return { classes, encodedStyle };
@@ -20,6 +28,12 @@ const { classes, encodedStyle } = declareStyle({
     padding: "1.5em",
     willChange: "filter",
     transition: "filter 300ms",
+  },
+  "logo": {
+    filter: "grayscale(100%)",
+    // "&:hover": {
+    //   filter: "grayscale(0%)",
+    // }
   }
 });
 
@@ -34,7 +48,7 @@ function App() {
           <img src={viteLogo} class={classes.logo} alt="Vite logo" />
         </a>
         <a href="https://solidjs.com" target="_blank">
-          <img src={solidLogo} class={`${classes.logo} ${classes.solid}`} alt="Solid logo" />
+          <img src={solidLogo} class={`${classes.logo} ${classes.thing}`} alt="Solid logo" />
         </a>
       </div>
       <h1>Vite + Solid</h1>
