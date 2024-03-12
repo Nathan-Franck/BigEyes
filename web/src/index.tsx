763b848594034cba63a99eb10346d576d91d1efe
Nathan Franck <nathanfranck1@gmail.com>
2024-03-12 05:23:27 +0000

    no idea why Apptsx isn 't updating like it should

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -6,5 +6,4 @@ import App from './App'
 
 const root = document.getElementById('root')
 
-// TODO: Figure out why this app isn't rendering - is it something wrong with my environment? Might have to test on the laptop...
-render(() => <><App/></>, root!)
+render(() => <><div>What</div><App/></>, root!)
