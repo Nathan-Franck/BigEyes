6f654b0230cffa3552dd0ea60c409a79e0bc5210
Nathan Franck <nathanfranck1@gmail.com>
2024-03-12 04:41:00 +0000

    Uncommenting more things about the vite app that aren't breaking, just down to whatever's happening in the zig interface module

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,53 +1,51 @@
 import { createSignal } from 'solid-js'
-// import solidLogo from './assets/solid.svg'
-// import viteLogo from '/vite.svg'
-// import { declareStyle } from "./declareStyle"
+import solidLogo from './assets/solid.svg'
+import viteLogo from '/vite.svg'
+import { declareStyle } from "./declareStyle"
 // import {} from "./zigWasmInterface"
 
-// const { classes, encodedStyle } = declareStyle({
-//   solidLogo: {
-//     height: "6em",
-//     padding: "1.5em",
-//     willChange: "filter",
-//     transition: "filter 300ms",
-//   },
-//   logo: {
-//     filter: "grayscale(100%)",
-//     "&:hover": {
-//       filter: "grayscale(0%)",
-//     }
-//   }
-// });
-
-const Tester = <div>Hello worldo!</div>
+const { classes, encodedStyle } = declareStyle({
+  solidLogo: {
+    height: "6em",
+    padding: "1.5em",
+    willChange: "filter",
+    transition: "filter 300ms",
+  },
+  logo: {
+    filter: "grayscale(100%)",
+    "&:hover": {
+      filter: "grayscale(0%)",
+    }
+  }
+});
 
 function App() {
-  // const [count, setCount] = createSignal(0)
-  // <style>{encodedStyle}</style>
-  // <div>
-  //   <a href="https://vitejs.dev" target="_blank">
-  //     <img src={viteLogo} class={classes.logo} alt="Vite logo" />
-  //   </a>
-  //   <a href="https://solidjs.com" target="_blank">
-  //     <img src={solidLogo} class={`${classes.logo} ${classes.solidLogo}`} alt="Solid logo" />
-  //   </a>
-  // </div>
-  // <h1>Vite + Solid</h1>
-  // <div class="card">
-  //   <button onClick={() => setCount((count) => count + 1)}>
-  //     count is {count()}
-  //   </button>
-  //   <p>
-  //     Edit <code>src/App.tsx</code> and save to test HMR, sir
-  //   </p>
-  // </div>
-  // <p class="read-the-docs">
-  //   Click on the Vite and Solid logos to learn more
-  // </p>
+  const [count, setCount] = createSignal(0)
 
   return (
     <>
       <div>Hello World!</div>
+      <style>{encodedStyle}</style>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} class={classes.logo} alt="Vite logo" />
+        </a>
+        <a href="https://solidjs.com" target="_blank">
+          <img src={solidLogo} class={`${classes.logo} ${classes.solidLogo}`} alt="Solid logo" />
+        </a>
+      </div>
+      <h1>Vite + Solid</h1>
+      <div class="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count()}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR, sir
+        </p>
+      </div>
+      <p class="read-the-docs">
+        Click on the Vite and Solid logos to learn more
+      </p>
     </>
   )
 }
