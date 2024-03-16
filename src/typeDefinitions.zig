275af6f539dfe56364e4776480355c8ebbd7a390
Nathan Franck <nathanfranck1@gmail.com>
2024-03-16 17:10:11 -0600

    Zig -> typescript enum support (test? Is this how zig actually parses enums?)

--- src/typeDefinitions.zig
+++ src/typeDefinitions.zig
@@ -24,6 +24,16 @@ pub inline fn typescriptTypeOf(comptime from_type: anytype, comptime options: st
                 typescriptTypeOf(pointer.child, .{}) ++ "[]",
             else => "unknown",
         },
+        .Enum => |enum_info| {
+            var result: []const u8 = &.{};
+            for (enum_info.fields, 0..) |field, i| {
+                result = result ++ std.fmt.comptimePrint("{s}\"{s}\"", .{
+                    if (i == 0) "" else " | ",
+                    field.name,
+                });
+            }
+            return result;
+        },
         .Union => |union_info| {
             var result: []const u8 = &.{};
             for (union_info.fields, 0..) |field, i| {
@@ -74,7 +84,7 @@ pub inline fn typescriptTypeOf(comptime from_type: anytype, comptime options: st
             }
             return std.fmt.comptimePrint("({s}) => {s}", .{ params, typescriptTypeOf(function_info.return_type.?, .{}) });
         },
-        else => std.fmt.comptimePrint("unknown /** zig type is {any} **/", .{ @typeInfo(from_type) }),
+        else => std.fmt.comptimePrint("unknown /** zig type is {any} **/", .{@typeInfo(from_type)}),
     };
 }
 
