39a5b5bc7aeb7cf0f44693abba36498a6d55ba80
Nathan Franck <nathanfranck1@gmail.com>
2024-04-08 23:11:04 -0600

    Type definition for this structure works, now need a separate implementation to actually fill in the data before JSON serialization

--- src/typeDefinitions.zig
+++ src/typeDefinitions.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const utils = @import("./nodeUtils.zig");
 
 pub inline fn typescriptTypeOf(comptime from_type: anytype, comptime options: struct { first: bool = false }) []const u8 {
     return comptime switch (@typeInfo(from_type)) {
@@ -89,36 +90,92 @@ pub inline fn typescriptTypeOf(comptime from_type: anytype, comptime options: st
     };
 }
 
+pub const TypedArrayReference = struct {
+    type: []const u8,
+    pointer: u32,
+    length: u32,
+};
+
 /// Recursively explores a structure for slices that are compatible with javascript typed arrays,
 /// and replaces with a special shape that the front-end can directly use.
-export fn DeepSliceToTypedArray(t: type) type {
+/// Supported typed arrays are:
+/// Float32Array
+/// Float64Array
+/// Int8Array
+/// Int16Array
+/// Int32Array
+/// NOT Uint8Array - since in Zig this is put aside for strings
+pub fn DeepTypedArrayReferences(t: type) type {
     return switch (@typeInfo(t)) {
+        else => t,
+        .Array => |a| switch (a.child) {
+            f32, f64, i8, i16, i32, u16, u32 => TypedArrayReference,
+            else => @Type(.{ .Array = .{
+                .len = a.len,
+                .sentinel = a.sentinel,
+                .child = DeepTypedArrayReferences(a.child),
+            } }),
+        },
         .Pointer => |p| switch (p.size) {
-                .Many => switch (p.child)
-                    f32 => 
-
-                
-
-                else => return t;
-            }
+            .Many, .Slice => switch (p.child) {
+                f32, f64, i8, i16, i32, u16, u32 => TypedArrayReference,
+                else => @Type(.{ .Pointer = utils.copyWith(p, .{ .child = DeepTypedArrayReferences(p.child) }) }),
+            },
+            else => @Type(.{ .Pointer = utils.copyWith(p, .{ .child = DeepTypedArrayReferences(p.child) }) }),
         },
         .Struct => |s| blk: {
-            var fields = []std.builtin.Type.StructField{};
+            var fields: []const std.builtin.Type.StructField = &.{};
             for (s.fields) |field| {
-                const field_type = DeepSliceReference(field.type);
+                const field_type = DeepTypedArrayReferences(field.type);
                 fields = fields ++ .{std.builtin.Type.StructField{
+                    .is_comptime = field.is_comptime,
+                    .name = field.name,
+                    .type = field_type,
                     .alignment = @alignOf(field_type),
                     .default_value = if (field_type == field.type)
                         field.default_value
                     else
                         null,
-                    .is_comptime = field.is_comptime,
-                    .name = field.name,
-                    .type = field_type,
                 }};
             }
-            break :blk std.builtin.Type.Struct{ .fields = fields,  };
+            break :blk @Type(.{ .Struct = utils.copyWith(s, .{ .fields = fields }) });
         },
+        .Union => |u| blk: {
+            var fields: []const std.builtin.Type.UnionField = &.{};
+            for (u.fields) |field| {
+                fields = fields ++ .{utils.copyWith(field, .{
+                    .type = DeepTypedArrayReferences(field.type),
+                })};
+            }
+            break :blk @Type(.{ .Union = utils.copyWith(u, .{ .fields = fields }) });
+        },
+    };
+}
+
+test "DeepTypedArrayReferences" {
+    {
+        const actual = DeepTypedArrayReferences([]f32);
+        const expected = TypedArrayReference;
+        try std.testing.expect(actual == expected);
+    }
+
+    { // Struct
+        const t = DeepTypedArrayReferences(struct { a: []f32, b: []const u8 });
+        _ = t{ .a = .{ .type = "Float32Array", .pointer = 0, .length = 0 }, .b = "Hello World!" };
+    }
+
+    { // Slice
+        const t = DeepTypedArrayReferences(struct { a: []const []f32 });
+        _ = t{ .a = &.{.{ .type = "Float32Array", .pointer = 0, .length = 0 }} };
+    }
+
+    { // Array
+        const t = DeepTypedArrayReferences(struct { a: [2][]f32 });
+        _ = t{ .a = .{ .{ .type = "Float32Array", .pointer = 0, .length = 0 }, .{ .type = "Float32Array", .pointer = 0, .length = 0 } } };
+    }
+    { // Union
+        const t = DeepTypedArrayReferences(union { a: []f32, b: []const u8 });
+        _ = t{ .a = .{ .type = "Float32Array", .pointer = 0, .length = 0 } };
     }
 }
 
