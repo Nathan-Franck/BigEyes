961982fdbbd84b03bdbc12f36d0b2ed7f4552bc0
Nathan Franck <nathanfranck1@gmail.com>
2024-07-20 11:01:40 -0600

    Some fancy grid traversal iterator

--- src/raytrace.zig
+++ src/raytrace.zig
@@ -1,4 +1,6 @@
 const zm = @import("zmath/main.zig");
+const std = @import("std");
+const Vec = zm.Vec;
 
 pub const Triangle = struct {
     a: zm.Vec,
@@ -38,4 +40,70 @@ pub fn rayTriangleIntersection(ray: Ray, triangle: Triangle) ?struct { distance:
         return .{ .distance = t };
     }
     return null;
+}
+
+pub const GridTraversal = struct {
+    current: Vec,
+    step: Vec,
+    tDelta: Vec,
+    tMax: Vec,
+    end: Vec,
+
+    pub fn init(start: Vec, end: Vec) GridTraversal {
+        const dir = end - start;
+
+        const step = @select(f32, dir > @as(Vec, @splat(0)), @as(Vec, @splat(1)), @as(Vec, @splat(-1)));
+
+        const tDelta = @select(f32, dir != @as(Vec, @splat(0)), @abs(@as(Vec, @splat(1)) / dir), @as(Vec, @splat(std.math.inf(f32))));
+
+        const startFloor = @floor(start);
+        const tMax = @select(f32, step > @as(Vec, @splat(0)), (startFloor + @as(Vec, @splat(1)) - start) * tDelta, (start - startFloor) * tDelta);
+
+        return GridTraversal{
+            .current = @floor(start),
+            .step = step,
+            .tDelta = tDelta,
+            .tMax = tMax,
+            .end = end,
+        };
+    }
+
+    pub fn next(self: *GridTraversal) ?Vec {
+        if (@reduce(.And, @abs(self.current - @floor(self.end)) < @as(Vec, @splat(epsilon)))) {
+            return null;
+        }
+
+        const result = self.current;
+
+        // Calculate boolean comparisons directly from self.tMax
+        const x_smallest = self.tMax[0] <= self.tMax[1] and self.tMax[0] <= self.tMax[2];
+        const y_smallest = self.tMax[1] <= self.tMax[0] and self.tMax[1] <= self.tMax[2];
+        const z_smallest = self.tMax[2] <= self.tMax[0] and self.tMax[2] <= self.tMax[1];
+
+        // Cast to boolean vectors
+        const mask_x = @as(@Vector(4, bool), @splat(x_smallest));
+        const mask_y = @as(@Vector(4, bool), @splat(y_smallest));
+        const mask_z = @as(@Vector(4, bool), @splat(z_smallest));
+
+        // Use boolean vectors in @select
+        const update_x = @select(f32, mask_x, @as(Vec, @Vector(4, f32){ 1, 0, 0, 0 }), @as(Vec, @splat(0)));
+        const update_y = @select(f32, mask_y, @as(Vec, @Vector(4, f32){ 0, 1, 0, 0 }), @as(Vec, @splat(0)));
+        const update_z = @select(f32, mask_z, @as(Vec, @Vector(4, f32){ 0, 0, 1, 0 }), @as(Vec, @splat(0)));
+
+        const update = update_x + update_y + update_z;
+
+        self.current += self.step * update;
+        self.tMax += self.tDelta * update;
+
+        return result;
+    }
+};
+test "Grid Traversal Iterator" {
+    const start = Vec{ 0, 0, 0, 0 };
+    const end = Vec{ 5, 3, 2, 0 };
+    var traversal = GridTraversal.init(start, end);
+
+    while (traversal.next()) |cell| {
+        std.debug.print("Visiting cell: ({d}, {d}, {d})\n", .{ cell[0], cell[1], cell[2] });
+    }
 }
